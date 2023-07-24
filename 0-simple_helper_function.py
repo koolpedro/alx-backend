0cf6c2e0ee8bb032fdbf1d5bb164ecda28a03cdb
koolpedro <panyakorah@gmail.com>
2023-07-24 04:16:30 -0700

    fix code error

--- 0-simple_helper_function.py
+++ 0-simple_helper_function.py
@@ -5,7 +5,11 @@
 from typing import Tuple
 
 
-def index_range(page: int, page_size: int) -> Tuple[int, int]:
-    """ Returns a tuple containing a start and end index.
-    """
-    return ((page - 1) * page_size, page * page_size)
+def index_range(page, page_size):
+    if page <= 0 or page_size <= 0:
+        raise ValueError("Both page and page_size must be positive integers.")
+
+    start_index = (page - 1) * page_size
+    end_index = start_index + page_size
+
+    return start_index, end_index
