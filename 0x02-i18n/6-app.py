d43daf057c7d0cd6c706e5916c23f42f89b69ab4
koolpedro <panyakorah@gmail.com>
2023-08-05 09:03:47 -0700

    fix code task

--- 0x02-i18n/6-app.py
+++ 0x02-i18n/6-app.py
@@ -1,39 +1,55 @@
 #!/usr/bin/env python3
-from flask import Flask, request, render_template
 
+from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
-SUPPORTED_LOCALES = ['en', 'fr', 'es', 'de']
+SUPPORTED_LOCALES = ['en', 'fr']
 
 
-def get_locale():
-    # Check if locale from URL parameters is supported
-    url_locale = request.args.get('locale')
-    if url_locale and url_locale in SUPPORTED_LOCALES:
-        return url_locale
 
-    # Check if locale from user settings is supported
-    user_settings_locale = get_user_settings_locale()  
+def get_locale() -> str:
 
-    # Replace this with your function to get user settings
-    if user_settings_locale and user_settings_locale in SUPPORTED_LOCALES:
-        return user_settings_locale
+    """
+    Get the user's preferred locale based on priority:
+    1. Locale from URL parameters
+    2. Locale from user settings
+    3. Locale from request header
+    4. Default locale
 
-    # Check if locale from request header is supported
-    request_header_locale = request.headers.get('Accept-Language')
-    if request_header_locale:
-        request_header_locale = request_header_locale.split(',')[0].split('-')[0]
-        if request_header_locale in SUPPORTED_LOCALES:
-            return request_header_locale
+    Returns:
+        str: The user's preferred locale.
+    """
+    # Check if the locale is provided in the URL parameters
+    locale = request.args.get('locale')
+    if locale and locale in SUPPORTED_LOCALES:
+        return locale
 
-    # Return the default locale if none of the above options are available
-    return 'en'
+    # Check if the locale is set in the user settings (you should implement this logic)
+    # Replace `get_user_locale()` with the appropriate function to get the user's locale from settings
+    user_locale = get_user_locale()
+    if user_locale and user_locale in SUPPORTED_LOCALES:
+        return user_locale
+
+    # Check if the locale is provided in the request header
+    # For example, if you are using 'Accept-Language' header to specify locale
+    header_locale = request.headers.get('Accept-Language')
+    if header_locale:
+        # Extract the first part of the header value as it contains the locale
+        header_locale = header_locale.split(',')[0].split(';')[0].lower()
+        if header_locale in SUPPORTED_LOCALES:
+            return header_locale
+
+    # If no preferred locale is found, return the default locale
+    return 'en'  # Replace 'en' with your default locale
 
 @app.route('/')
 def index():
-    preferred_locale = get_locale()
-    return render_template('6-index.html', preferred_locale=preferred_locale)
+    """
+    Render the index template with the user's preferred locale.
+    """
+    locale = get_locale()
+    return render_template('6-index.html', locale=locale)
 
 if __name__ == '__main__':
-    app.run(host="127.0.0.1", port="5000")
+    app.run(host = "0.0.0.0", port = "5000")
